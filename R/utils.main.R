64e1f5da03efe2ec46ea7bc7ec3daa46e8851b1a
Luke Smith <lukedansmi@sbcglobal.net>
2018-03-19 02:03:41 -0500

    More efficient, style-consistent code. Will re-write later on.

--- R/utils.main.R
+++ R/utils.main.R
@@ -9,7 +9,7 @@ captureArea <- function(capture) {
   range_end   <- capture$selection[[1L]]$range$end[[1L]]
 
   # Stop if range is < 2 lines
-  if ((range_end - range_start) < 1L) stop("At least two lines must be selected", .call = FALSE)
+  if ((range_end - range_start) < 1L) warning("At least two lines must be selected", .call = FALSE)
 
   # Dump contents and use highlighted lines as names.
   contents        <- capture$contents[range_start:range_end]
@@ -17,31 +17,29 @@ captureArea <- function(capture) {
   return(contents)
 }
 
-findRegEx <- function(find, where, ignore_case = FALSE, wrap_space = FALSE) {
+findRegEx <- function(find, where, ...) {
 
-  # Wrap the 'find' input in leading/trailing spaces if 'wrap_space' == TRUE
-  find <- if (wrap_space) paste0("\\s", find, "\\s") else find
-
-  # Find matches, extract positions, find furthest <-, get rows/cols to align.
-  positions    <- regexec(find, where, ignore.case = ignore_case)
+  # Find and extract first match positions on each line (if any),
+  positions    <- regexec(find, where, ...)
   matched_rows <- which(unlist(lapply(positions, `!=`, e2 = -1L)))
   positions    <- positions[matched_rows]
 
   # Stop if only one or fewer matches
-  if (length(positions) <= 1L) stop("Did not match 2 or more characters to be aligned.")
+  if (length(positions) <= 1L) warning("Did not match 2 or more regular expressions to align.")
 
+  # Get range of highlighted lines, where matches were found,
+  # and furthest position (to the right)
   highlighted_lines <- as.integer(names(where))
   matched_cols      <- vapply(positions, `[[`, integer(1L), 1L)
   which_max_col     <- which.max(matched_cols)
-
-  furthest.row    <- highlighted_lines[matched_rows[which_max_col]]
-  furthest.column <- max(matched_cols)
+  # furthest_row    <- highlighted_lines[matched_rows[which_max_col]]
+  furthest_column <- max(matched_cols)
 
   return(list(matched_rows      = matched_rows,
               matched_cols      = matched_cols,
               highlighted_lines = highlighted_lines,
               which_max_col     = which_max_col,
-              furthest.column   = furthest.column))
+              furthest_column   = furthest_column))
 }
 
 assembleInsert <-function(info) {
@@ -50,17 +48,17 @@ assembleInsert <-function(info) {
   matched_cols      <- info$matched_cols
   highlighted_lines <- info$highlighted_lines
   which_max_col     <- info$which_max_col
-  furthest.column   <- info$furthest.column
+  furthest_column   <- info$furthest_column
 
   # Find the rows to align and the current column position of each regEx match.
-  rows.to.align    <- highlighted_lines[matched_rows[-which_max_col]]
-  columns.to.align <- matched_cols[-which_max_col]
+  rows_to_align    <- highlighted_lines[matched_rows[-which_max_col]]
+  columns_to_align <- matched_cols[-which_max_col]
 
   # Set location for spaces to be inserted.
-  location <- Map(c, rows.to.align, columns.to.align)
+  location <- Map(c, rows_to_align, columns_to_align)
 
   # Find and set the number of spaces to insert on each line.
-  text.num <- furthest.column - columns.to.align
+  text.num <- furthest_column - columns_to_align
   text     <- vapply(text.num,
                      function(x) paste0(rep(" ", x), collapse = ""),
                      character(1L))
